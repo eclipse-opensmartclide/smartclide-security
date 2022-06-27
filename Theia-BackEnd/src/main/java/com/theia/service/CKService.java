326569d77b2abb456ebd9ab55512fa8d480276e3
Philippe Krief <philippe.krief@eclipse-foundation.org>
2022-06-27 19:00:51 +0200

    Added copyrights
    Signed-off-by: Philippe Krief <philippe.krief@eclipse-foundation.org>

--- Theia-BackEnd/src/main/java/com/theia/service/CKService.java
+++ Theia-BackEnd/src/main/java/com/theia/service/CKService.java
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ * Copyright (C) 2021-2022 CERTH
+ * 
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ******************************************************************************/
 package com.theia.service;
 
 import com.github.mauricioaniche.ck.CK;
